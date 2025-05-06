b2bec2b657e5ce5c3759607ede2fcd1732efdf27
siaciss <ciss.sia@gmail.com>
2025-05-06 22:57:30 +0000

    gabarits, css, add attributs

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,13 +1,32 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Band(models.Model):
+
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+
     name = models.fields.CharField(max_length=100)
-    genre = models.fields.CharField(max_length=50)
+    genre = models.fields.CharField(choices=Genre.choices,max_length=5)
     biography = models.fields.CharField(max_length=1000)
     year_formed = models.fields.IntegerField(validators=[MinValueValidator(1900), MaxValueValidator(2021)])
     active = models.fields.BooleanField(default=True)
     official_homepage = models.fields.URLField(null=True, blank=True)
 
 class Listing(models.Model):
+
+    class Type(models.TextChoices):
+        disques = 'Records'
+        vêtements = 'Clothing'
+        affiches = 'Posters'
+        divers = 'Miscellaneous'
+
     title = models.fields.CharField(max_length=100)
+    description = models.fields.CharField(max_length=1000)
+    sold = models.fields.BooleanField(default=True)
+    year = models.fields.IntegerField(null=True)
+    type = models.fields.CharField(choices=Type.choices, max_length=20)
+    
